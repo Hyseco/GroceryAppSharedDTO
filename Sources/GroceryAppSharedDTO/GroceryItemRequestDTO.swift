ae92cdc612d32a86bfada503d2cb36b6e05b0fe7
Damien Theys <damien@hyseco.be>
2023-11-24 14:33:29 +0100

    s

--- Sources/GroceryAppSharedDTO/GroceryItemRequestDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryItemRequestDTO.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+public struct GroceryItemRequestDTO: Codable {
+    public let title: String
+    public let price: Double
+    public let quantity: Int
+    
+    public init(title: String, price: Double, quantity: Int) {
+        self.title = title
+        self.price = price
+        self.quantity = quantity
+        
+    }
+}
